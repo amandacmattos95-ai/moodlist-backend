90f85533310ac362f34a3256491619277e57bc79
amandacmattos95-ai <amandacmattos95@gmail.com>
2025-08-30 21:49:41 +1000

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from spotipy.oauth2 import SpotifyOAuth
 
 app = Flask(__name__)
 
-# Seus dados do Spotify Developer
 CLIENT_ID = "7cb553bcc9504b03a00f05c7f87492db"
-CLIENT_SECRET = "7effe28a63644aaa1841e20b7f63accf"
-REDIRECT_URI = "https://moodlist.me/callback"
+CLIENT_SECRET = "7effe28a633644aaa1841e20b7f63acf"
+REDIRECT_URI = "https://moodlist-backend.onrender.com/callback"
 
 sp_oauth = SpotifyOAuth(
     client_id=CLIENT_ID,
@@ -27,10 +26,13 @@ def login():
 
 @app.route('/callback')
 def callback():
-    code = request.args.get('code')
+    code = request.args.get("code")
     token_info = sp_oauth.get_access_token(code)
-    return f"✅ Login feito! Seu Access Token é: {token_info['access_token']}"
+    access_token = token_info['access_token']
 
+    sp = spotipy.Spotify(auth=access_token)
+    user_profile = sp.current_user()
+    return f"Hello, {user_profile['display_name']}! 🎵"
 # Necessário para rodar no Render
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
